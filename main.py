15d1b37ae948847c6cbabf48fc6794e07b385fd7
Parisa80p <162550919+Parisa80p@users.noreply.github.com>
2024-05-27 23:19:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,5 @@ while(cap.isOpened()):
         # write the flipped frame
         out.write(frame)
 
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-      
+       
 
